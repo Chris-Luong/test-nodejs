6553adff858d765f9dd9f10d32391bda676193a7
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2025-01-29 23:52:01 +1100

    feat (Server): use Express.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 // Tutorial:
 // https://medium.com/@ibrahimhz/creating-your-first-backend-with-node-js-step-by-step-guide-892769af4cb0
 
-const http = require('http');
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, Express.js Server!</h1>");
+});
 
-	res.write('<h1>Hello, Node.js HTTP Server!</h1>');
-	res.end();
-})
-
-const port = 3000;
-
-server.listen(port, () => {
-	console.log(`Node.js HTTP Server is running on port ${port}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port", port);
 });
